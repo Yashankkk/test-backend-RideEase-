b2ac28c2367b1b0373bf51730a8c6d7a16a90faf
Yashankkk <126323177+Yashankkk@users.noreply.github.com>
2025-03-26 10:23:27 +0530

    initial commit

--- routes/api/Auth.route.js
+++ routes/api/Auth.route.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 const register = require("../../controllers/auth/registration");
 const loginform = require('../../controllers/auth/login');
 const contactme= require('../../controllers/auth/contact');
+const google = require("../../controllers/auth/google")
 
 router.post("/registration", register);
 router.post("/login", loginform);
 router.post("/contact", contactme);
-// router.get("/google", google);
+router.post("/google", google);
 
 module.exports = router;
